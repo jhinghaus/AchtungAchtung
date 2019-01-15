d9afbc1a5bf708ccff0413b37cf870828785727b
jhinghaus <jhinghaus@gmail.com>
2019-01-15 15:18:09 +0100

    correct import naming

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -9,15 +9,15 @@ import json
 class CollectTC(unittest.TestCase):
 
     def test_get(self):
-        alerts = achtungachtung.alertcollector.getAlerts()
+        alerts = alertcollector.getAlerts()
         # there should be at least the static one
         self.assertGreaterEqual(len(alerts),1)
-        alerts = achtungachtung.alertcollector.getAlerts(area='StaticArea')
+        alerts = alertcollector.getAlerts(area='StaticArea')
         self.assertEqual(len(alerts), 1, 'filtered too much')
-        alerts = achtungachtung.alertcollector.getAlerts(area='DiesenOrtGibtEsNichtDaBinIchSicher')
+        alerts = alertcollector.getAlerts(area='DiesenOrtGibtEsNichtDaBinIchSicher')
         self.assertEqual(len(alerts), 0, 'did not filter')
 
     def test_encoder(self):
-        alerts = achtungachtung.alertcollector.getAlerts(area='StaticArea')
-        dump = json.dumps(alerts, cls=achtungachtung.alertcollector.AlertEncoder)
+        alerts = alertcollector.getAlerts(area='StaticArea')
+        dump = json.dumps(alerts, cls=alertcollector.AlertEncoder)
         self.assertIn('StaticArea', dump)
